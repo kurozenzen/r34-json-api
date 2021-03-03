30f5169c4b8b4a934776a11a601ef5d97943fbd6
kurozenzen <kurozenzen.@gmail.com>
2021-03-03 02:08:35 +0100

    Added seeking for videos

--- routing/images.js
+++ routing/images.js
@@ -2,18 +2,42 @@ const express = require("express");
 const https = require("https");
 const imageRouter = express.Router();
 
-imageRouter.get("/", function(req, res) {
+const mediaHeaders = [
+  "connection",
+  "content-range",
+  "accept-ranges",
+  "content-length",
+];
+
+imageRouter.get("/", function (req, res) {
   if (!req.query.url) {
     res.sendStatus(404);
   }
 
-  const request = https.get(req.query.url, function(response) {
-    res.setHeader("Content-Type", response.headers["content-type"]);
-    res.setHeader("Cache-Control", "max-age=31557600");
-    response.pipe(res);
-  });
+  const request = https.get(
+    req.query.url,
+    {
+      headers: mediaHeaders.reduce((res, headername) => {
+        if (headername && req.headers[headername]) {
+          res[headername] = req.headers[headername];
+        }
+        return res;
+      }, {}),
+    },
+    function (response) {
+      Object.entries(response.headers).forEach(function (header) {
+        if (header[0] && header[1] && mediaHeaders.includes(header[0])) {
+          res.setHeader(header[0], header[1]);
+        }
+      });
 
-  request.on("error", function(e) {
+      res.setHeader("Content-Type", response.headers["content-type"]);
+      res.setHeader("Cache-Control", "max-age=31557600");
+      response.pipe(res);
+    }
+  );
+
+  request.on("error", function (e) {
     console.error(e);
   });
 });
